0747b60b845b4e8f1f22b48bc3d65fad26fa2776
time su <timesuss@hotmail.com>
2013-12-27 17:18:30 +0800

    new file:   13-rember-letcc.scm 	modified:   2-member.scm

---
+++ 13-rember-letcc.scm
@@ -0,0 +1,46 @@
+;;13-rember-letcc.scm
+
+(define rember
+  (lambda (a lat)
+    (letrec
+	((R (lambda (lat)
+	      (cond
+	       ((null? lat) (quote()))
+	       ((eq? (car lat) a) (cdr lat))
+	       (else (cons (car lat)
+			   (R (cdr lat))))))))
+      (R lat))))
+
+(rember 'a '(a b c))
+
+
+;;rember-beyond-first 
+;;The function rember-beyond-first takes
+;;an atom a and a lat and , if a occurs in the
+;;lat, removes all atoms from the lat beyond
+;;and including the first occurence of a.
+
+(define rember-beyond-first
+  (lambda (a lat)
+    (letrec
+	((R 
+	  (lambda (lat)
+	    (cond
+	     ((null? lat) (quote()))
+	     ((eq? (car lat) a)
+	      (quote()))
+	     (else
+	      (cons (car lat)
+		    (R (cdr lat))))))))
+	  (R lat))))
+
+(rember-beyond-first 'others '(noodles
+			       spaghetti spatzle bean-thread
+			       roots
+			       potatoes yam
+			       others
+			       rice))
+
+;;rember-upto-last
+;;Takes an atom a and a lat and removes all the
+;;atoms from the lat up 
